4ce6923ae09e5f9c3c2b38619883ed81ed17ec31
Ashish Kumar Patel <ashish_3805@hotmail.com>
2018-01-14 20:55:50 +0530

    made cards fragment

--- app/src/main/java/com/boophee/boophee/MainActivity.java
+++ app/src/main/java/com/boophee/boophee/MainActivity.java
@@ -1,5 +1,6 @@
 package com.boophee.boophee;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,7 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SwipeCardsFragment.OnFragmentInteractionListener {
 
     private TextView mTextMessage;
 
@@ -18,13 +20,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    //mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -36,9 +38,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        displaySwipeCards(savedInstanceState);
+
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    private void displaySwipeCards(Bundle savedInstanceState) {
+        if (findViewById(R.id.fragmentHolder) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            SwipeCardsFragment swipeCardsFragment = new SwipeCardsFragment();
+            swipeCardsFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragmentHolder, swipeCardsFragment).commit();
+        }
     }
 
 }
